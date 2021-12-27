450c1f58c859f922475b595ba039369bf0e7e4c8
vova <est@mail.ru>
2021-12-27 20:46:59 +0500

    add entering name

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+"use-strict";
 function rain (amount = 1000) {
     let falling = document.querySelector('.falling');
     for (let i = 1; i <= amount; i++) {
@@ -8,4 +9,14 @@ function rain (amount = 1000) {
         falling.append(fallingStar);
     }
 }
+let ask = null
+while (ask == null || ask.length > 9 || ask == '') {
+    ask = prompt('Введите ваше имя, не превышайте 9 букв')
+    if(ask) {
+        ask = ask.replace(/\s/gm, ''); 
+        continue;
+    }
+}
+let userName = document.querySelector('.user-name');
+userName.innerHTML = ask;
 requestAnimationFrame(() => rain(500));
